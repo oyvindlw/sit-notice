227224cd850017f280cbe7273a356f2120cb38f0
oyvindlw <oyvind.westrum@gmail.com>
2016-12-17 21:59:34 +0100

    La til main osv.

--- sit.py
+++ sit.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
 from lxml import html
 from gi.repository import Notify
 import requests
 
-page = requests.get('https://www.sit.no/middag/elgeseter')
-tree = html.fromstring(page.content)
-sit = tree.xpath('//li[@class="dishes__dishes__dish dishes__dishes__dish--old-day"]/text()')
+def main():
+    page = requests.get('https://www.sit.no/middag/elgeseter')
+    tree = html.fromstring(page.content)
+    sit = tree.xpath('//li[@class="dishes__dishes__dish dishes__dishes__dish--old-day"]/text()')
+    
+    sit = [s.strip() for s in sit]
 
-sit = [s.strip() for s in sit]
-
-Notify.init ("Hello world")
-Hello=Notify.Notification.new ("Hello world",
+    Notify.init ("Hello world")
+    Hello=Notify.Notification.new ("Hello world",
                                "This is an example notification.",
                                "dialog-information")
-Hello.show ()
+    Hello.show ()
+
+if __name__ == "__main__":
+    main()
