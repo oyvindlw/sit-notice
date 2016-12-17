d4c2265caecaef3ea5b8c2b56fb63b4bc9f170fa
oyvindlw <oyvind.westrum@gmail.com>
2016-12-17 21:47:44 +0100

    Test

---
+++ sit.py
@@ -0,0 +1,15 @@
+from lxml import html
+from gi.repository import Notify
+import requests
+
+page = requests.get('https://www.sit.no/middag/elgeseter')
+tree = html.fromstring(page.content)
+sit = tree.xpath('//li[@class="dishes__dishes__dish dishes__dishes__dish--old-day"]/text()')
+
+sit = [s.strip() for s in sit]
+
+Notify.init ("Hello world")
+Hello=Notify.Notification.new ("Hello world",
+                               "This is an example notification.",
+                               "dialog-information")
+Hello.show ()
