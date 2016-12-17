79ab2c2e75475b48d4a24c7347196e5f432ed37a
oyvindlw <oyvind.westrum@gmail.com>
2016-12-17 23:10:26 +0100

    bugs

--- sit.py
+++ sit.py
@@ -10,7 +10,6 @@ sys.setdefaultencoding('utf-8')
 ukedag = datetime.datetime.today().weekday()
 
 
-
 def main():
     page = requests.get('https://www.sit.no/middag/elgeseter')
     tree = html.fromstring(page.content)
@@ -22,19 +21,21 @@ def main():
     lunsjMessage = ""
     
     if ukedag == 0:
-        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[0], sit[2])
+        lunsjMessage = "{} og {}.".format(sit[0], sit[2])
     elif ukedag == 1:
-        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[4], sit[6])
+        lunsjMessage = "{} og {}.".format(sit[4], sit[6])
     elif ukedag == 2:
-        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[8], sit[10])
+        lunsjMessage = "{} og {}.".format(sit[8], sit[10])
     elif ukedag == 3:
-        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[12], sit[14])
+        lunsjMessage = "{} og {}.".format(sit[12], sit[14])
     elif ukedag == 4:
-        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[16], sit[18])
+        lunsjMessage = "{} og {}.".format(sit[16], sit[18])
     else:
         lunsjMessage = "Ingen lunsj idag."
     
-    Hello=Notify.Notification.new (lunsjMessage)
+    Hello=Notify.Notification.new ("Dagens Lunsj",
+                                    lunsjMessage,
+                                    )
     Hello.show ()
 
 if __name__ == "__main__":
