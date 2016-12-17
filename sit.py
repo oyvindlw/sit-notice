7a977b7d53960306714a1f88127be4d45ebcd34c
oyvindlw <oyvind.westrum@gmail.com>
2016-12-17 23:00:15 +0100

    bug fix

--- sit.py
+++ sit.py
@@ -3,6 +3,9 @@ from lxml import html
 import requests
 import datetime
 from gi.repository import Notify
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 ukedag = datetime.datetime.today().weekday()
 
@@ -16,21 +19,22 @@ def main():
     sit = [s.strip() for s in sit]
     
     Notify.init ("sit-notice")
+    lunsjMessage = ""
     
     if ukedag == 0:
-        lunsjMessage = ("Dagens lunsj: {} og {}.".format(sit[0], sit[2]))
+        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[0], sit[2])
     elif ukedag == 1:
-        lunsjMessage = ("Dagens lunsj: {} og {}.".format(sit[4], sit[6]))
+        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[4], sit[6])
     elif ukedag == 2:
-        lunsjMessage = ("Dagens lunsj: {} og {}.".format(si[8], sit[10]))
+        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[8], sit[10])
     elif ukedag == 3:
-        lunsjMessage = ("Dagens lunsj: {} og {}.".format(sit[12], sit[14]))
+        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[12], sit[14])
     elif ukedag == 4:
-        lunsjMessage = ("Dagens lunsj: {} og {}.".format(sit[16], sit[18]))
+        lunsjMessage = "Dagens lunsj: {} og {}.".format(sit[16], sit[18])
     else:
-        lunchMessage = "Ingen lunsj idag."
-
-    Hello=Notify.Notification.new ("test")
+        lunsjMessage = "Ingen lunsj idag."
+    
+    Hello=Notify.Notification.new (lunsjMessage)
     Hello.show ()
 
 if __name__ == "__main__":
