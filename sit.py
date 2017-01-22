67f9127016d44f9f0447aa1e33e3cc83ec61b189
oyvindlw <oyvind.westrum@gmail.com>
2017-01-22 12:41:37 +0100

    div

--- sit.py
+++ sit.py
@@ -2,7 +2,9 @@
 from lxml import html
 import requests
 import datetime
+
 from gi.repository import Notify
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -16,20 +18,21 @@ def main():
     sit = tree.xpath('//li[@class="dishes__dishes__dish dishes__dishes__dish--old-day"]/text()')
     
     sit = [s.strip() for s in sit]
-    
+    sit = list(filter(None, sit))
+
     Notify.init ("sit-notice")
     lunsjMessage = ""
     
     if ukedag == 0:
-        lunsjMessage = "{} og {}.".format(sit[0], sit[2])
+        lunsjMessage = "{} og {}.".format(sit[0], sit[1])
     elif ukedag == 1:
-        lunsjMessage = "{} og {}.".format(sit[4], sit[6])
+        lunsjMessage = "{} og {}.".format(sit[2], sit[3])
     elif ukedag == 2:
-        lunsjMessage = "{} og {}.".format(sit[8], sit[10])
+        lunsjMessage = "{} og {}.".format(sit[4], sit[5])
     elif ukedag == 3:
-        lunsjMessage = "{} og {}.".format(sit[12], sit[14])
+        lunsjMessage = "{} og {}.".format(sit[6], sit[7])
     elif ukedag == 4:
-        lunsjMessage = "{} og {}.".format(sit[16], sit[18])
+        lunsjMessage = "{} og {}.".format(sit[8], sit[9])
     else:
         lunsjMessage = "Ingen lunsj idag."
     
